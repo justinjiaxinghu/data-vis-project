0692d51db24738ff811603c2f630e3b6af5033cd
Hu, Justin Jiaxing <jhu357@gatech.edu>
2020-11-21 19:14:24 -0500

    init stuff changes

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-window.onload = start;
+var width = 1000;
+var height = 1000;
 
-function start () {
+d3.csv("books.csv", function (csv) {
+
+    for (var i = 0; i < csv.length; i++) {
+        csv[i].Author = String(csv[i].Author);
+        csv[i].Price = Number(csv[i].Price);
+        csv[i].Year = Number(csv[i].Year);
+        csv[i].Genre = String(csv[i].Genre);
+    }
+
     
-}
+});
