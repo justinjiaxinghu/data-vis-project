f023eef397aac23e3d6dc3db89f43320b957128b
sukriti1 <sukriti1.bhardwaj@gmail.com>
2020-11-26 02:27:26 -0500

    init line graph

--- main.js
+++ main.js
@@ -12,20 +12,45 @@ d3.csv("books.csv", function (csv) {
     /* domain for x axis aka years that data was recorded */
     var xDomain = ["2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
 
-    var testData = d3.nest().key(function (d) {
-        return d.Genre;
-    })
-    .key(function (d) {
-        return d.Year;
-    })
-    .object(csv);
+    csv.forEach(function(d) {
+        d.Genre = d.Genre
+        d.Year = +d.Year;
+        d.value = d.length;
+    });
 
-    console.log(testData);
+    var nest = d3.nest()
+	  .key(function(d){
+	    return d.Genre;
+	  })
+	  .key(function(d){
+	  	return d.Year;
+	  })
+      .entries(csv)
+    
+
+    var testData = d3.nest()
+                    .key(function (d) {
+                        console.log(d.Genre)
+                        return d.Genre;
+                    })
+                    .key(function (d) {
+                        return d.Year;
+                    })
+                    .rollup(function (d) {
+                        return d.length;
+                    })
+                    .object(csv);
 
     var min = 1000; //min value for y axis
     var max = 0;    //max value for y axis
     var fictionYearMap = new Map();    //map of values for non fiction books, ie 2009 : 2
     var nonFictionYearMap = new Map(); //map of values for fiction books, ie 2009 : 2
+
+    var colors = d3.scaleOrdinal()
+    .domain(["strawberry", "grape"])
+    .range(["#EF5285", "#88F284"]);
+
+
 
     var nonFictionKeys = Object.keys(testData["Non Fiction"]);
     for (var i = 0; i < nonFictionKeys.length; i++) {
@@ -39,7 +64,7 @@ d3.csv("books.csv", function (csv) {
         }
         nonFictionYearMap.set(currKey, val);
     }
-    console.log(nonFictionYearMap);
+
 
     var fictionKeys = Object.keys(testData["Fiction"]);
     for (var i = 0; i < fictionKeys.length; i++) {
@@ -54,58 +79,80 @@ d3.csv("books.csv", function (csv) {
         fictionYearMap.set(currKey, val);
     }
 
-    var xScale = d3.scaleOrdinal().domain(xDomain).range(xRange);
-    var xAxis = d3.axisBottom().scale(xScale);
+    var x = d3.scaleOrdinal().domain(xDomain).range(xRange);
+    var xAxis = d3.axisBottom().scale(x);
     
-    var yScale = d3.scaleLinear().domain([min - 3, max + 3]).range([500, 0]);
-    var yAxis = d3.axisLeft().scale(yScale);
+    var y = d3.scaleLinear()
+                .domain([min - 3, max + 3])
+                .range([500, 0]);
+    var yAxis = d3.axisLeft().scale(y);
+
+    var chart = d3.select("#chart")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);   
+                
+
+                
+    var valueLine = d3.line()
+                .x(function(d) { return x(d.Year); })
+                .y(function(d) { return y(+d.length); })
+
+    console.log("val",valueLine)
+            
     
-    
-
-    var chart = d3
-                    .select("#chart")
-                    .append("svg")
-                    .attr("width", width)
-                    .attr("height", height);
-    var circles = chart
-                    .selectAll("circle")
-                    .data(csv)
-                    .enter()
-                    .append("circle")
-                    .attr("id", function(d,i) {
-                        return i;
-                    })
-                    .attr("stroke", "black")
-                    .attr("cx", function (d) {
-                        return xScale(d.Year) + 150;
-                    })
-                    .attr("cy", function (d) {
-                        /* todo change to match preprocessed data */
-                        // return yScale(d.Author) + 6;
-                        return 0;
-                    })
-                    .attr("r", 2);
+    chart.selectAll("circle")
+        .data(csv)
+        .enter()
+        .append("circle")
+        .attr("id", function(d,i) {
+            return i;
+        })
+        .attr("stroke", "black")
+        .attr("cx", function (d) {
+            return x(d.Year) + 150;
+        })
+        .attr("cy", function (d) {
+            /* todo change to match preprocessed data */
+            //return yScale(d.Author) + 6;
+            return 0;
+        })
+        .attr("r", 2);
 
     
-    chart 
-                    .append("g") 
-                    .attr("transform", "translate(150," + (550) + ")")
-                    .call(xAxis) 
-                    .append("text")
-                    .attr("class", "label")
-                    .attr("x", width - 16)
-                    .attr("y", -6)
-                    .style("text-anchor", "end");
+    chart.append("g") 
+        .attr("transform", "translate(150," + (550) + ")")
+        .call(xAxis) 
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width - 16)
+        .attr("y", -6)
+        .style("text-anchor", "end");
 
-    chart
-                    .append("g")
-                    .attr("transform", "translate(150, 50)")
-                    .call(yAxis)
-                    .append("text")
-                    .attr("class", "label")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em")
-                    .style("text-anchor", "end");
-
+    chart.append("g")
+        .attr("transform", "translate(150, 50)")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end"); 
+        
+    var genreGroups = chart.selectAll(".genreGroups")
+	    .data(nest)
+	    .enter()
+	    .append("g")
+        .attr("stroke", function(d){ return colors(d.key)});
+    
+    var paths = genreGroups.selectAll(".line")
+        .data(function(d){ return d.values})
+        .enter()
+        .append("path");
+    paths
+	    .attr("d", function(d){
+		  return valueLine(d.values)
+		})
+		.attr("class", "line")
+		.style("stroke-dasharray")
 });
